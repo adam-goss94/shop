66c1b259ad9762d53d8d88e2a6e0460e3393587a
Adam Goss <adam.goss94@gmail.com>
2020-08-14 14:36:00 +0200

    Change products data

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -3,12 +3,28 @@ export const initialState = {
     {
       id: '1',
       title: 'Nazwa produktu',
+      category: 'T-shirt',
       description: 'Opis produktu',
       price: '21,15',
     },
     {
       id: '2',
       title: 'Nazwa produktu',
+      category: 'T-shirt',
+      description: 'Opis produktu',
+      price: '33,33',
+    },
+    {
+      id: '3',
+      title: 'Nazwa produktu',
+      category: 'Hoodie',
+      description: 'Opis produktu',
+      price: '21,15',
+    },
+    {
+      id: '4',
+      title: 'Nazwa produktu',
+      category: 'Hoodie',
       description: 'Opis produktu',
       price: '33,33',
     },
