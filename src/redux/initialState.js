88cd44cb968e6b3da2453001b96e22f66d8fe193
Adam Goss <adam.goss94@gmail.com>
2020-08-14 13:27:19 +0200

    Add example data to initalState

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -1,9 +1,21 @@
 export const initialState = {
-  posts: {
-    data: {},
-    loading: {
-      active: false,
-      error: false,
+  products: [
+    {
+      id: '1',
+      title: 'Nazwa produktu',
+      description: 'Opis produktu',
+      price: '21,15',
     },
+    {
+      id: '2',
+      title: 'Nazwa produktu',
+      description: 'Opis produktu',
+      price: '33,33',
+    },
+  ],
+
+  loading: {
+    active: false,
+    error: false,
   },
 };
