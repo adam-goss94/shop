164a58a52fb2e436ef20c68378f772dc7ce7bd6d
Adam Goss <adam.goss94@gmail.com>
2020-08-31 09:38:19 +0200

    Fix products view styles, add product content

--- src/components/common/Product/Product.js
+++ src/components/common/Product/Product.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import { Card, Button } from 'react-bootstrap';
 import styles from './Product.module.scss';
+import { Link } from 'react-router-dom';
 
 const Component = ({id, title, category, description, price, image}) => (
   <div className={styles.root}>
-    <div>{title}</div>
-    <div>{category}</div>
-    <div>{description}</div>
-    <div>{price}</div>
-    <div>{image}</div>
+    <Card key={id} className={styles.cards} as={Link} to={`/product/${id}`}>
+      <Card.Img variant="top" src={image} alt={title} className={styles.image} />
+      <Card.Body className={styles.cardBody}>
+        <Card.Title className={styles.title}>{title}</Card.Title>
+        <Card.Subtitle className={styles.price}>{price}$</Card.Subtitle>
+        <Button variant="primary" className={styles.priceButton}>
+          Details
+        </Button>
+      </Card.Body>
+    </Card>
   </div>
 );
 
