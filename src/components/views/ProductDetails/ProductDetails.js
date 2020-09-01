6976dd7370a8af6a27d9e95bab70010764cd223f
Adam Goss <adam.goss94@gmail.com>
2020-09-01 19:06:13 +0200

    Add button event handler

--- src/components/views/ProductDetails/ProductDetails.js
+++ src/components/views/ProductDetails/ProductDetails.js
@@ -5,14 +5,20 @@ import PropTypes from 'prop-types';
 // import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
 import { connect } from 'react-redux';
 import { getProduct } from '../../../redux/productsRedux';
+import { addToCart } from '../../../redux/cartRedux';
 import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import styles from './ProductDetails.module.scss';
 
 class Component extends React.Component {
 
+  addToCartHandler = () => {
+    this.props.addToCart(this.props.product);
+  };
+
   render() {
     const { product } = this.props;
+    const { addToCartHandler } = this;
     return (
       <div className={styles.root}>
         <h2>Additional Details</h2>
@@ -27,7 +33,7 @@ class Component extends React.Component {
               {product.description}
             </div>
             <div className={styles.submit}>
-              <Button as={Link} to="/Cart">Add to cart</Button>
+              <Button as={Link} to="/Cart" onClick={addToCartHandler}>Add to cart</Button>
             </div>
           </div>
         </div>
@@ -42,13 +48,18 @@ Component.propTypes = {
       id: PropTypes.string,
     }),
   }),
+  addToCart: PropTypes.func,
 };
 
 const mapStateToProps = (state, props) => ({
   product: getProduct(state, props.match.params.id),
 });
 
-const Container = connect(mapStateToProps)(Component);
+const mapDispatchToProps = (dispatch) => ({
+  addToCart: (payload) => dispatch(addToCart(payload)),
+});
+
+const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
 
 export {
   //Component as ProductDetails,
