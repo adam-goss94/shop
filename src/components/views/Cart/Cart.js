5959c8de57e0683c501f5b312a820b4baaba54ed
Adam Goss <adam.goss94@gmail.com>
2020-09-01 19:45:25 +0200

    Add form

--- src/components/views/Cart/Cart.js
+++ src/components/views/Cart/Cart.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getCart } from '../../../redux/cartRedux';
+import { Form, FormGroup, Label, Input } from 'reactstrap';
 // import { connect } from 'react-redux';
 // import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
 
@@ -9,18 +10,38 @@ import styles from './Cart.module.scss';
 
 const Component = ( cart ) => {
   const cartProducts = cart.cart.data;
-  console.log(cartProducts);
+
   return (
     <div className={styles.root}>
       <h2>Cart</h2>
+      <div>
+        <Form>
+          <FormGroup>
+            <Label for="clientName">Name</Label>
+            <Input type="text" name="client" id="clientName" placeholder="John Doe" />
+          </FormGroup>
+          <FormGroup>
+            <Label for="clienEmail">Email</Label>
+            <Input type="email" name="email" id="clienEmail" placeholder="Johndo@example.com" />
+          </FormGroup>
+          <FormGroup>
+            <Label for="address">Street</Label>
+            <Input type="text" name="address" id="address" placeholder="Example 12/23" />
+          </FormGroup>
+          <FormGroup>
+            <Label for="notes">Street</Label>
+            <Input type="text" name="notes" id="notes" placeholder="Additional details" />
+          </FormGroup>
+        </Form>
+      </div>
       {cartProducts.map((product) => (
         <div key={product._id} className={styles.product}>
           <img src={product.img} alt={product.title} />
           <p className={styles.title}>{product.title}</p>
           <span className={styles.price}>{product.price}$</span>
           <div className={styles.amount}>
-            <p>Amount:</p>
-            <input type='number' min='1' step='1' id='amount'></input>
+            <Label for="amount">Amount:</Label>
+            <input type='number' min='1' step='1' id="amount"></input>
           </div>
         </div>
       ))
