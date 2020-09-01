1bcba202dde57793ebaf8a6e2f655014519c8cc3
Adam Goss <adam.goss94@gmail.com>
2020-09-01 16:49:29 +0200

    Fix errorx

--- src/redux/store.js
+++ src/redux/store.js
@@ -10,12 +10,6 @@ const reducers = {
   products: productsReducer,
 };
 
-// add blank reducers for initial state properties without reducers
-Object.keys(initialState).forEach(item => {
-  if (typeof reducers[item] == 'undefined') {
-    reducers[item] = (statePart = null) => statePart;
-  }
-});
 
 const combinedReducers = combineReducers(reducers);
 
