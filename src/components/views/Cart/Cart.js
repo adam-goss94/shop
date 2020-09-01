9adf2976095db9667cefd54a96fdd4f62941fc5c
Adam Goss <adam.goss94@gmail.com>
2020-09-01 19:06:43 +0200

    Add cart component

--- src/components/views/Cart/Cart.js
+++ src/components/views/Cart/Cart.js
@@ -1,35 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import { connect } from 'react-redux';
+import { getCart } from '../../../redux/cartRedux';
 // import { connect } from 'react-redux';
 // import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
 
 import styles from './Cart.module.scss';
 
-const Component = ({className, children}) => (
-  <div className={styles.root}>
-    <h2>Cart</h2>
-    {children}
-  </div>
-);
+const Component = ( cart ) => {
+  const cartProducts = cart.cart.data;
+  console.log(cartProducts);
+  return (
+    <div className={styles.root}>
+      <h2>Cart</h2>
+      {cartProducts.map((product) => (
+        <div key={product._id} className={styles.product}>
+          <img src={product.img} alt={product.title} />
+          <p className={styles.title}>{product.title}</p>
+          <span className={styles.price}>{product.price}$</span>
+          <div className={styles.amount}>
+            <p>Amount:</p>
+            <input type='number' min='1' step='1' id='amount'></input>
+          </div>
+        </div>
+      ))
+      }
 
-Component.propTypes = {
-  children: PropTypes.node,
-  className: PropTypes.string,
+    </div>
+  );
 };
 
-// const mapStateToProps = state => ({
-//   someProp: reduxSelector(state),
-// });
+Component.propTypes = {
+  cart: PropTypes.object,
+};
 
-// const mapDispatchToProps = dispatch => ({
-//   someAction: arg => dispatch(reduxActionCreator(arg)),
-// });
+const mapStateToProps = (state) => ({
+  cart: getCart(state),
+});
 
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+const Container = connect(mapStateToProps)(Component);
 
 export {
-  Component as Cart,
-  // Container as Cart,
+  //Component as Cart,
+  Container as Cart,
   Component as CartComponent,
 };
